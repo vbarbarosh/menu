ad954bee45a864ffe02415999593656185a26510
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-09-06 01:39:15 +0300

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,11 @@ function render_config(mode)
             rules: [
                 {
                     test: /\.js$/,
-                    exclude: /(node_modules|bower_components)/,
+                    exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env']
+                            presets: ['@babel/preset-env'],
                         },
                     },
                 },
